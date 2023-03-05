692f14a1f9462cad835bc24c6cc9ea7c75f447dc
Terentev Dmitry <zabey52@gmail.com>
2023-03-05 08:17:01 +0300

    p1

--- 01/main.cpp
+++ 01/main.cpp
@@ -16,25 +16,31 @@ enum class PiramidLevels
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    std::srand(std::time(nullptr)); // Начальное значение для генератора случайных чисел
+    //std::srand(std::time(nullptr)); // Начальное значение для генератора случайных чисел
 
-    int arrSize = 3 + rand() % 11;
+    //int arrSize = 3 + rand() % 17;
     
+    //int arr[] = { 1, 3, 6, 5, 9, 8 };
+    
+    //int arr[] = {94, 67, 18, 44, 55, 12, 6, 42};
+    int arr[] = {16, 11, 9, 10, 5, 6, 8, 1, 2, 4};
+    int arrSize = std::size(arr);
+
     // Создаем массив
-    int* arr = new int[arrSize] {};
+    //int* arr = new int[arrSize] {};
     
     // Заполняем массив рандомными числами
-    for (int i = 0; i < arrSize; i++)
-    {
-        arr[i] = rand() % 55;
-    }
+    //for (int i = 0; i < arrSize; i++)
+    //{
+    //    arr[i] = rand() % 55;
+    //}
     
     PrintArray(arr, arrSize);
  
     PrintPiramid(arr, arrSize);
     
-    delete[] arr;  // Удаляем массив
-    arr = nullptr;  
+    //delete[] arr;  // Удаляем массив
+    //arr = nullptr;  
     
     std::cout << "\nHello World!\n";
 }
--- 01/print.cpp
+++ 01/print.cpp
@@ -15,72 +15,89 @@ void PrintArray(int* arr, const int arrSize)
     std::cout << std::endl;
 };
 
-//void PrintValueArray(int* arr, const int arrSize, int arrIndex)
-//{
-//    std::cout << "Уровень: " << levelsOfPiramid << ". Left(" << arr[arrIndex] << ")" << ". Значение: " << arr[2 * i + 1] << std::endl;
-//    std::cout << "Уровень: " << levelsOfPiramid << ". Right(" << arr[arrIndex] << ")" << ". Значение: " << arr[2 * i + 2] << std::endl;
-//
-//};
-
 
 void PrintPiramid(int* arr, const int arrSize)
 {
-    int piramidLevel = 0;
+    int piramidLevelNow = 0;
 
     std::cout << "Пирамида:\n";
-    std::cout << "Уровень: " << piramidLevel++ << ". Root. Значение: " << arr[0] << std::endl;
+    std::cout << "piramidLevelNow: " << piramidLevelNow << std::endl;
+
+    std::cout << "Уровень: " << piramidLevelNow++ << ". Root. Значение: " << arr[0] << std::endl;
+    //std::cout << "Уровень: " << piramidLevelNow << ". Left(" << arr[0] << ")" << ". Значение: " << arr[1] << std::endl;
+    //std::cout << "Уровень: " << piramidLevelNow << ". Right(" << arr[0] << ")" << ". Значение: " << arr[2] << std::endl;
         
     int levelsOfPiramid = arrSize / log2(arrSize);
 
     std::cout << "levelsOfPiramid: " << levelsOfPiramid << std::endl;
 
-    do
+    //std::cout << "piramidLevelNow: " << piramidLevelNow << std::endl;
+    
+    int count = piramidLevelNow;
+    
+    int count2 = arrSize;
+     
+    for (int i = 0; i <= count2 / 2 ; i++)
     {
-        ///if( => arrSize)
-
-
-        for (int i = 0; i < std::pow(2, piramidLevel); i++)
+        if (count2 > 1)
         {
-
-            int count = piramidLevel;
-            int kkk = std::pow(2, count);
-
-            while (kkk > 1)
-            {
-                std::cout << "Уровень: " << piramidLevel - 1 << ". Left(" << arr[i] << ")" << ". Значение: " << arr[2 * i + 1] << std::endl;
-
-                std::cout << "Уровень: " << piramidLevel - 1 << ". Right(" << arr[i] << ")" << ". Значение: " << arr[2 * i + 2] << std::endl;
-                count = kkk--;
-            }
-            
-
-            
-
-            //std::cout << "Уровень: " << i + 1 << "Left(" << 2 * i + 1 << ")" << "Значение: " << arr[i] << std::endl;
+            std::cout << "Уровень: " << piramidLevelNow << ". Left(" << arr[i] << ")" << ". Значение: " << arr[2 * i + 1] << std::endl;
+            //count2 -= 1;
+        }
+        if (count2 > 1)
+        {
+        std::cout << "Уровень: " << piramidLevelNow << ". Right(" << arr[i] << ")" << ". Значение: " << arr[2 * i + 2] << std::endl;
+        //count2 -= 1;
+        }
 
 
-
-            //if (arrSize = )
-            //    std::cout << "Уровень: " << i;
-
-            //
-            //if ((2*i + 1) / 2)
-            //
-            //std::cout << " ";
-            //std::cout << arr[i] << std::endl;
-
-            ////2i+1 - нечет
-            ////2i+2 - чет
+    }
 
 
 
 
 
-        };
-        piramidLevel++;
 
-    } while (piramidLevel <= levelsOfPiramid);
 
+
+
+
+
+    
+    //do
+    //{
+    //    std::cout << "piramidLevelNow: " << piramidLevelNow << std::endl;
+
+
+    //    int kkk = std::pow(2, piramidLevelNow);
+
+
+
+
+    //    for (int i = 0; i < kkk; ++i)
+    //    {
+    //        
+    //        
+
+    //        //while (kkk > 1)
+    //        //{
+    //            std::cout << "Уровень: " << piramidLevelNow << ". Left(" << arr[(i - 1) / 2] << ")" << ". Значение: " << arr[2 * i + 1] << std::endl;
+
+    //            std::cout << "Уровень: " << piramidLevelNow << ". Right(" << arr[(i - 2) / 2] << ")" << ". Значение: " << arr[2 * i + 2] << std::endl;
+    //            kkk--;
+    //       // }
+    //     
+
+
+
+
+    //    };
+    //    piramidLevelNow++;
+
+    //} while (piramidLevelNow <= levelsOfPiramid);
+
+    
+    
     std::cout << std::endl;
 
 
